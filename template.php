b0e51f95ee3f398a37ae7841b1c05c03ef0808e0
Breezewish <me@breeswish.org>
2015-08-09 00:18:08 +0800

    inner-page navigation

--- template.php
+++ template.php
@@ -7,6 +7,13 @@
  * @see https://drupal.org/node/1728096
  */
 
+const sse_menu_navigation = 'menu-sse-navigation-menu';
+const sse_menu_footer = 'menu-sse-footer-menu';
+const sse_menu_main = 'menu-sse-main-menu';
+
+/**
+ * 返回该主题基目录的 URI
+ */
 function sse_asset_path()
 {
   static $path = null;
@@ -52,9 +59,13 @@ function sse_get_current_section()
     $route = menu_get_active_trail();
     if (count($route) >= 2) {
       // $route[0] 是首页
-      $entity = menu_fields_load_by_mlid($route[1]['mlid']);
-      $menu_id = $entity->wrapper()->field_id->value();
-      $section = $menu_id;
+      if (isset($route[1]['menu_name']) && $route[1]['menu_name'] === sse_menu_navigation) {
+        $entity = menu_fields_load_by_mlid($route[1]['mlid'])->wrapper();
+        $menu_id = $entity->field_id->value();
+        $section = $menu_id;
+      } else {
+        $section = 'default';
+      }
     } else {
       $section = 'default';
     }
@@ -80,51 +91,150 @@ function sse_breadcrumb_output()
 }
 
 /**
- * 获取经过处理的导航栏
- * @param  boolean $extra 是否要包含新闻和通知
+ * 将一颗菜单树递归地转换为包含 ID 字段的菜单树
  */
-function sse_get_navigations($extra = false)
+function sse_process_menu_tree_with_id(&$tree)
 {
-  static $menu_name = 'menu-sse-navigation-menu';
+  $items = [];
+  foreach ($tree['below'] as &$item) {
+    if ($item['link']['hidden'] !== true) {
+      $items[] = sse_process_menu_tree_with_id($item);
+    }
+  }
+  unset($item);
+
+  $ret = [
+    'href' => $tree['link']['href'],
+    'title' => $tree['link']['title'],
+    'items' => $items
+  ];
+  $entity = menu_fields_load_by_mlid($tree['link']['mlid']);
+  if ($entity !== null) {
+    $entity = $entity->wrapper();
+    if (isset($entity->field_id)) {
+      $ret['id'] = $entity->field_id->value();
+    }
+  }
+
+  return $ret;
+}
+
+/**
+ * 获取包含 ID 字段的菜单树
+ */
+function sse_get_menu_tree_with_id($menu_name)
+{
   $result = [];
   $menu_tree = menu_tree_all_data($menu_name);
-  foreach ($menu_tree as &$section) {
-    if ($section['link']['hidden'] === true) {
+  foreach ($menu_tree as &$tree) {
+    if ($tree['link']['hidden'] === true) {
       continue;
     }
-    $entity = menu_fields_load_by_mlid($section['link']['mlid']);
-    $menu_id = $entity->wrapper()->field_id->value();
-    $items = [];
-    foreach ($section['below'] as &$item) {
-      $items[] = [
-        'href' => url($item['link']['href']),
-        'text' => $item['link']['title'],
-      ];
-    }
-    unset($item);
-    $result[] = [
-      'text' => $section['link']['title'],
-      'id' => $menu_id,
-      'items' => $items
-    ];
+    $result[] = sse_process_menu_tree_with_id($tree);
   }
-  unset($section);
+  unset($tree);
   return $result;
 }
 
 /**
- * 返回 footer 导航
+ * 获得一颗数组形式的 taxonomy_tree，该形式和 sse_get_menu_tree_with_id() 一致
  */
-function sse_footer_navigation_output()
+function sse_get_taxonomy_tree($taxonomy_name)
+{
+  $result = [];
+  $vocabulary = taxonomy_vocabulary_machine_name_load($taxonomy_name);
+  // 为了使用 entity_uri 接口，需要最后一个参数为 true
+  $taxonomy_tree = i18n_taxonomy_localize_terms(taxonomy_get_tree($vocabulary->vid, 0, null, true));
+  foreach ($taxonomy_tree as &$item) {
+    $result[] = [
+      // 使用 entity_uri 接口，这样这个 URI 会被 ssetaxonomy 插件转换成正确的地址
+      'href' => entity_uri('taxonomy_term', $item)['path'],
+      'title' => $item->name
+    ];
+  }
+  unset($item);
+  return $result;
+}
+
+/**
+ * 获得顶部导航条
+ */
+function sse_get_navigation_main($is_frontpage = false)
+{
+  $result = [];
+  $menu_tree = menu_tree_all_data(sse_menu_main);
+  foreach ($menu_tree as &$item) {
+    if ($item['link']['hidden'] === true) {
+      continue;
+    }
+    $curitem = [
+      'href' => $item['link']['href'],
+      'title' => $item['link']['title'],
+      'items' => []
+    ];
+    $entity = menu_fields_load_by_mlid($item['link']['mlid']);
+    if ($entity === null) {
+      $result[] = $curitem;
+      continue;
+    }
+    $entity = $entity->wrapper();
+    if (isset($entity->field_id)) {
+      $curitem['id'] = $entity->field_id->value();
+    }
+    if (!isset($entity->field_type)) {
+      $result[] = $curitem;
+      continue;
+    }
+    $ref_type = $entity->field_type->value();
+    if ($ref_type === 'none') {
+      // 无引用：没有子项
+      $result[] = $curitem;
+      continue;
+    }
+    // 有引用
+    if (!isset($entity->field_from)) {
+      $result[] = $curitem;
+      continue;
+    }
+    $ref_target = $entity->field_from->value();
+
+    if ($ref_type === 'menu') {
+      // 菜单引用
+      $subitems = sse_get_menu_tree_with_id($ref_target);
+    } else if ($ref_type === 'taxonomy') {
+      $subitems = sse_get_taxonomy_tree($ref_target);
+    } else {
+      $result[] = $curitem;
+      continue;
+    }
+
+    // 是否以子项代替当前项
+    if ($entity->field_expand->value() === true && $is_frontpage === false) {
+      foreach ($subitems as $subitem) {
+        $result[] = $subitem;
+      }
+    } else {
+      $curitem['items'] = $subitems;
+      $result[] = $curitem;
+    }
+  }
+  unset($item);
+  return $result;
+}
+
+/**
+ * 返回 footer 导航 HTML
+ */
+function sse_navigation_footer_output()
 {
   $output = '';
-  $navi = sse_get_navigations(false);
+  $navi = sse_get_menu_tree_with_id(sse_menu_navigation);
   foreach ($navi as &$section) {
-    $output .= '<div class="footer__navi__section footer__navi__section__'.$section['id'].'">';
-    $output .= '<h1 class="footer__navi__section__title">'.$section['text'].'</h1>';
+    $output .= '<div class="footer__navi__section footer__navi__section--'.check_plain($section['id']).'">';
+    $output .= '<h1 class="footer__navi__section__title">'.check_plain($section['title']).'</h1>';
     $output .= '<ul class="footer__navi__section__items">';
     foreach ($section['items'] as &$item) {
-      $output .= '<li class="footer__navi__section__item"><a href="'.$item['href'].'" target="_self">'.$item['text'].'</a></li>';
+      $output .= '<li class="footer__navi__section__item"><a href="'.url($item['href']).'" target="_self">'.check_plain($item['title']).'</a></li>';
     }
     unset($item);
     $output .= '</ul></div>';
@@ -133,15 +243,78 @@ function sse_footer_navigation_output()
   return $output;
 }
 
+function sse_navigation_output_tree(&$tree, $level, $is_frontpage = false)
+{
+  $active_section = sse_get_current_section();
+  if (count($tree) === 0) {
+    return '';
+  }
+  // 是否首页
+  $front_symbol = $is_frontpage ? 'f' : 'nf';
+  // 第几级菜单
+  $level_symbol = 'lv'.$level;
+
+  $append_class = function($base) use ($front_symbol, $level_symbol) {
+    return "$base--$front_symbol--$level_symbol";
+  };
+
+  $output = '<ul class="'.$append_class('nav__list').'">'."\n";
+  foreach ($tree as &$item) {
+    $has_subitems = (isset($item['items']) && count($item['items']) > 0);
+    $output .= '<li class="'.$append_class('nav__i');
+    if (isset($item['id'])) {
+      $output .= ' nav__i--section--'.check_plain($item['id']);
+      if ($item['id'] === $active_section) {
+        $output .= ' nav__i--active';
+      }
+    }
+    $output .= '"><a class="nav__l '.$append_class('nav__l').'" href="'.url($item['href']).'" target="_self">'.check_plain($item['title']);
+    // 如果是顶级菜单，则有子项的时候，显示 v 并且紧跟文字
+    if ($has_subitems && $level === 0) {
+      $output .= '<i class="icon-expand_more"></i>';
+    }
+    $output .= '</a>';
+    // 如果不是顶级菜单，则有子项的时候，显示 > 并且单独排列
+    if ($has_subitems && $level > 0) {
+      $output .= '<div class="nav__icon"><i class="icon-chevron_right"></i></div>';
+    }
+    // 增加子菜单
+    if ($has_subitems) {
+      $output .= '<div class="'.$append_class('nav__sublist').'">'.sse_navigation_output_tree($item['items'], $level + 1, $is_frontpage).'</div>';
+    }
+    $output .= '</li>'."\n";
+  }
+  unset($item);
+  $output .= "</ul>";
+  return $output;
+}
+
 /**
- * 渲染 footer 的语言项时增加一项「开发人员」
+ * 返回头部导航 HTML
+ */
+function sse_navigation_main_output()
+{
+  $is_frontpage = drupal_is_front_page();
+  $navi = sse_get_navigation_main($is_frontpage);
+  return sse_navigation_output_tree($navi, 0, $is_frontpage);
+}
+
+/**
+ * 渲染 footer 的语言项时增加 footer_menu
  */
 function sse_links__locale_block(&$vars)
 {
-  $vars['links']['contributors'] = [
-    'href' => '<front>',
-    'title' => '开发人员'
-  ];
+  $footer_menu = menu_tree_all_data(sse_menu_footer);
+  foreach ($footer_menu as $key => &$item) {
+    if ($item['link']['hidden'] === true) {
+      continue;
+    }
+    $vars['links'][$key] = [
+      'href' => $item['link']['href'],
+      'title' => $item['link']['title']
+    ];
+  }
+  unset($item);
   $content = theme_links($vars);
   return $content;
 }
@@ -154,7 +327,7 @@ function sse_has_sidenav()
   static $has_side_nav = null;
   if ($has_side_nav === null) {
     $route = menu_get_active_trail();
-    $has_side_nav = (count($route) >= 2 && $route[1]['menu_name'] === 'menu-sse-navigation-menu');
+    $has_side_nav = (count($route) >= 2 && isset($route[1]['menu_name']) && $route[1]['menu_name'] === sse_menu_navigation);
   }
   return $has_side_nav;
 }
@@ -170,23 +343,24 @@ function sse_sidenav_output()
   $route = menu_get_active_trail();
   $parent = $route[1];
   $param = [
-    'active_trail' => array($parent['plid']),
+    'active_trail' => [$parent['plid']],
     'only_active_trail' => false,
     'min_depth' => $parent['depth'] + 1,
     'max_depth' => $parent['depth'] + 1,
-    'conditions' => array('plid' => $parent['mlid']),
+    'conditions' => ['plid' => $parent['mlid']],
   ];
   $items = menu_build_tree($parent['menu_name'], $param);
-  $entity = menu_fields_load_by_mlid($parent['mlid']);
-  $menu_id = $entity->wrapper()->field_id->value();
-  $output = '<nav class="sidenav sidenav--section-'.$menu_id.'">';
-  $output .= '<h1 class="sidenav__title"><a href="'.$parent['link_path'].'" target="_self" class="sidenav__title__link">'. $parent['title'] .'</a></h1>';
+  $entity = menu_fields_load_by_mlid($parent['mlid'])->wrapper();
+  $menu_id = $entity->field_id->value();
+  $output = '<nav class="sidenav sidenav--section-'.check_plain($menu_id).'">';
+  $output .= '<h1 class="sidenav__title"><a href="'.url($parent['href']).'" target="_self" class="sidenav__title__link">'. check_plain($parent['title']) .'</a></h1>';
   $output .= '<div class="sidenav__edge"></div><ul class="sidenav__list">';
-  foreach ($items as $item) {
+  foreach ($items as &$item) {
     $output .= '<li class="sidenav__item'. ((count($route) >= 3 && $item['link']['mlid'] === $route[2]['mlid']) ? ' sidenav__item--active' : '') .'">';
-    $output .= '<a href="'.url($item['link']['link_path']).'" target="_self" class="sidenav__item__link">'.$item['link']['title'].'</a>';
+    $output .= '<a href="'.url($item['link']['href']).'" target="_self" class="sidenav__item__link">'.check_plain($item['link']['title']).'</a>';
     $output .= '</li>';
   }
+  unset($item);
   $output .= '</ul></nav>';
   return $output;
 }
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -5,11 +5,13 @@
         <section class="header__logo-container">
           <a href="<?php print $front_page ?>" class="header__logo"><img src="<?php print sse_asset_path() ?>/img/logo.png" alt="<?php print t('Home') ?>"/></a>
         </section>
-        <?php print render($page['header']); ?>
+        <nav class="header__nav">
+          <?php print sse_navigation_main_output(); ?>
+        </nav>
       </div>
     </header>
   </section>
-  
+
   <section class="page-row page-row-expanded">
     <div class="content<?php if (sse_has_sidenav()) print ' content-has-sidenav'; ?>">
       <div class="content-container clearfix">
@@ -19,9 +21,9 @@
           <a id="main-content"></a>
           <div class="typo">
             <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
-              <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-            <?php endif; ?>
+<?php if ($title): ?>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+<?php endif; ?>
             <?php print render($title_suffix); ?>
             <?php print $messages; ?>
             <?php print render($page['content']); ?>
@@ -34,7 +36,7 @@
   <section class="page-row">
     <footer class="footer">
       <div class="content-container clearfix">
-        <section class="footer__navi clearfix"><?php print sse_footer_navigation_output(); ?></section>
+        <section class="footer__navi clearfix"><?php print sse_navigation_footer_output(); ?></section>
         <section class="footer__info">
           <?php $footer_block = module_invoke('panels_mini', 'block_view', 'footer_info'); print $footer_block['content']; ?>
         </section>
