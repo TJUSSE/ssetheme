e5e809457b34899c14dd516e20ab9c5e453bd1b4
Breezewish <me@breeswish.org>
2015-09-17 09:41:20 +0800

    custom layout

---
+++ ds_layouts/intro_2col/intro-2col.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+// Add sidebar classes so that we can apply the correct width in css.
+if (($left && !$right) || ($right && !$left)) {
+  $classes = 'intro-layout--one-col ' . $classes;
+} else {
+  $classes = 'intro-layout--two-col ' . $classes;
+}
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+  <?php if ($left): ?>
+    <<?php print $left_wrapper ?> class="intro__left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper ?>>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <<?php print $right_wrapper ?> class="intro__right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper ?>>
+  <?php endif; ?>
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
