156cb56e4d0156c18f1d4c872de653b8dacd7219
Josh Ouyang <JoshOY789@gmail.com>
2015-09-21 14:13:54 +0800

    Apply slider to index page.

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -1,4 +1,7 @@
-  <header class="fp__header">
+  <script type="text/javascript" src="<?php print sse_asset_path() ?>/js/vendor/jquery.min.js"></script>
+  <script type="text/javascript" src="<?php print sse_asset_path() ?>/js/sse-slider.js" async="true"></script>
+
+  <header class="fp__header" style="z-index: 20000">
     <div class="content-container clearfix">
       <section class="fp__header__logo-container">
         <a href="<?php print $front_page ?>" class="header__logo"><img src="<?php print sse_asset_path() ?>/img/logo.png" alt="<?php print t('Home') ?>"/></a>
@@ -10,7 +13,44 @@
   </header>
 
   <section class="fp__slider">
-    <?php print render($page['content']); ?>
+    <!-- <?php print render($page['content']); ?> -->
+    <div class="sse-slider" id="slider-demo">
+      <ul>
+        <li>
+          <div class="slide" data-src="<?php print sse_asset_path() ?>/img/sse-slide-demo-large-1.png">
+            <div class="slide-content">
+              <h2><a href="#">CTFC2015 圆满落幕1</a></h2>
+              <p>由中国计算机协会主办, CCF容错计算专业委员会和同济大学承办的第十六届全国容错计算学术会议在同济大学中法中心召开。</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="slide" data-src="<?php print sse_asset_path() ?>/img/sse-slide-demo-large-2.png">
+            <div class="slide-content">
+              <h2><a href="#">CTFC2015 圆满落幕2</a></h2>
+              <p>由中国计算机协会主办, CCF容错计算专业委员会和同济大学承办的第十六届全国容错计算学术会议在同济大学中法中心召开。</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="slide" data-src="<?php print sse_asset_path() ?>/img/sse-slide-demo-large-3.png">
+            <div class="slide-content">
+              <h2><a href="#">CTFC2015 圆满落幕3</a></h2>
+              <p>由中国计算机协会主办, CCF容错计算专业委员会和同济大学承办的第十六届全国容错计算学术会议在同济大学中法中心召开。</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="slide" data-src="<?php print sse_asset_path() ?>/img/sse-slide-demo-large-4.png">
+            <div class="slide-content">
+              <h2><a href="#">CTFC2015 圆满落幕4</a></h2>
+              <p>由中国计算机协会主办, CCF容错计算专业委员会和同济大学承办的第十六届全国容错计算学术会议在同济大学中法中心召开。</p>
+            </div>
+          </div>
+
+        </li>
+      </ul>
+    </div>
   </section>
   
   <?php include __DIR__.'/footer.tpl.php' ?>
