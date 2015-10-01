f6fcbe9b514408db8e9ffc2740705b9de7564726
Breezewish <me@breeswish.org>
2015-10-01 20:44:01 +0800

    hide pinyin in ENGLISH

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -28,7 +28,7 @@
 <?php if ($title): ?>
             <h1 class="page__title title" id="page-title">
               <?php print $title; ?>
-              <?php if (isset($node) && $node->type === 'teacher_content' && !empty($node->field_teacher_pinyin)): ?>
+              <?php if (isset($node) && $node->type === 'teacher_content' && !empty($node->field_teacher_pinyin) && $GLOBALS['language']->language === 'zh-hans'): ?>
                 <small class="teacher__pinyin"><?php print $node->field_teacher_pinyin[LANGUAGE_NONE][0]['safe_value']; ?></small>
               <?php endif; ?>
             </h1>
